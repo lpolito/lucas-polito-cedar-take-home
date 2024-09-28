101f294c4b85cb8b1bf0f12b649bf8aeafafaf33
Lucas Polito <lucas.a.polito@gmail.com>
2024-09-28 17:30:56 -0400

    LayoutCard implementation
    fix layout positioning

--- src/app/invoice/[id]/payment/page.tsx
+++ src/app/invoice/[id]/payment/page.tsx
@@ -1,3 +1,9 @@
+import { LayoutCard } from "../../../../components/component-library";
+
 export default function Page() {
-  return <section className="flex flex-col items-center">payment page</section>;
+  return (
+    <section className="flex flex-col items-center md:pt-8">
+      <LayoutCard>payment form</LayoutCard>
+    </section>
+  );
 }
---
+++ src/components/component-library/LayoutCard.tsx
@@ -0,0 +1,14 @@
+interface LayoutCardProps {
+  children: React.ReactNode;
+}
+
+/**
+ * A card for content that bleeds to the edge on mobile, but has fixed width on larger screen sizes.
+ */
+export const LayoutCard = ({ children }: LayoutCardProps) => {
+  return (
+    <div className="w-full border-b border-solid border-[#E7E9EF] bg-white px-8 md:w-[576px] md:rounded-2xl md:border-b-0 md:px-12 md:py-4">
+      {children}
+    </div>
+  );
+};
