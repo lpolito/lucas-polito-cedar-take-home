6706979ad7e4d9eff07ea7ff7a698216ef7650fd
Lucas Polito <lucas.a.polito@gmail.com>
2024-09-30 10:25:28 -0400

    Refactor Stepper to use array of steps

--- src/components/component-library/Stepper.tsx
+++ src/components/component-library/Stepper.tsx
@@ -1,27 +1,40 @@
 import { Typography } from "./Typography";
 
-export interface StepperProps
-  extends Omit<React.HTMLAttributes<HTMLDivElement>, "children"> {
+export type Step = {
   stepNumber: number;
   label: string;
+};
+
+export interface StepperProps
+  extends Omit<React.HTMLAttributes<HTMLDivElement>, "children"> {
+  currentStep: number;
+  steps: Step[];
 }
 
-// TODO make Stepper take an array of steps, where the step number dictates the step displayed - better DX / more reusable
 export const Stepper = ({
-  stepNumber,
-  label,
+  currentStep,
+  steps,
   className,
   ...rest
-}: StepperProps) => (
-  <div
-    className={`flex flex-row items-center gap-4 py-5 ${className || ""}`}
-    {...rest}
-  >
-    <Typography className="bg-stepperBg flex h-6 w-6 items-center justify-center rounded-full font-bold text-white">
-      {stepNumber}
-    </Typography>
-    <Typography size="lg" className="font-bold">
-      {label}
-    </Typography>
-  </div>
-);
+}: StepperProps) => {
+  const activeStep = steps.find((step) => step.stepNumber === currentStep);
+  if (!activeStep) {
+    return null;
+  }
+
+  const { stepNumber, label } = activeStep;
+
+  return (
+    <div
+      className={`flex flex-row items-center gap-4 py-5 ${className || ""}`}
+      {...rest}
+    >
+      <Typography className="flex h-6 w-6 items-center justify-center rounded-full bg-stepperBg font-bold text-white">
+        {stepNumber}
+      </Typography>
+      <Typography size="lg" className="font-bold">
+        {label}
+      </Typography>
+    </div>
+  );
+};
