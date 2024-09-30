b441a5328a4c9408c8b2d71b914c2d8cef83e818
Lucas Polito <lucas.a.polito@gmail.com>
2024-09-29 22:12:56 -0400

    payment page basic, no stepper

--- src/app/invoice/[id]/payment/page.tsx
+++ src/app/invoice/[id]/payment/page.tsx
@@ -1,9 +1,25 @@
-import { LayoutCard } from "../../../../components/component-library";
+import { Button, LayoutCard } from "../../../../components/component-library";
+import { TextField } from "../../../../components/component-library/TextField";
 
 export default function Page() {
   return (
     <section className="flex flex-col items-center md:pt-8">
-      <LayoutCard>payment form</LayoutCard>
+      <LayoutCard>
+        <form className="flex flex-col gap-4">
+          <TextField label="Card number" fullWidth />
+          <div className="flex flex-row gap-4">
+            <TextField label="Expires (MM/YY)" fullWidth className="flex-1" />
+            <TextField
+              label="Security code (CVV)"
+              fullWidth
+              className="flex-1"
+            />
+          </div>
+          <TextField label="Name on card" fullWidth />
+          <TextField label="ZIP code" fullWidth />
+          <Button className="mb-8 mt-6">Continue</Button>
+        </form>
+      </LayoutCard>
     </section>
   );
 }
